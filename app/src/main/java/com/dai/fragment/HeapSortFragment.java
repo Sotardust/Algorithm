ea603bbe17f90351651f10f923633c794b805149
daihaitao <daihaitao@wanwujieshu.net>
2017-07-10 18:19:12 +0800

    添加 堆排序算法

--- app/src/main/java/com/dai/fragment/HeapSortFragment.java
+++ app/src/main/java/com/dai/fragment/HeapSortFragment.java
@@ -18,49 +18,53 @@ public class HeapSortFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_heap_sort, container, false);
-        final int[] ints = {23, 56, 45, 12, 78, 58,40};
+        final int[] ints = {3, 1, 8, 5, 2, 4, 9, 7, 6, 0};
         initView(view);
         sortTitle.setText("这是堆排序");
         initText.setText(convert(ints));
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                endText.setText(convert(ints));
                 heapSort(ints);
+                endText.setText(convert(ints));
             }
         });
         return view;
     }
 
-    /**
-     * 堆排序与快速排序，归并排序一样都是时间复杂度为O(N*logN)的几种常见排序方法
-     *
-     * @param ints
-     * @param root
-     */
-    private void heapAdjust(int[] ints, int root, int end) {
-        int temp = ints[root];
-        for (int i = root * 2 + 1; i < end; i++) {
-            if (ints[i] < ints[i + 1]) ++i;
-            if (temp > ints[i]) break;
-            ints[root] = ints[i];
-            root = i;
-        }
-        ints[root] = temp;
-    }
-
     private void heapSort(int[] ints) {
         for (int i = 0; i < ints.length / 2; i++) heapAdjust(ints, i, ints.length - 1);
-        System.out.println("convert(ints) = " + convert(ints));
         for (int i = ints.length - 1; i > 0; --i) {
-            int temp = ints[i];
-            ints[i] = ints[0];
-            ints[0] = temp;
-            heapAdjust(ints, 0, i-1);
-            System.out.println("                                    ");
-            System.out.println("convert(ints) = " + convert(ints));
+            //将堆顶结点与最后一个最后一个结点进行比较
+            swap(ints, i, 0);
+            //调整后可能违反堆的性质，需要对堆进行调整
+            heapAdjust(ints, 0, i - 1);
         }
+    }
 
+    /**
+     * 构建大根堆过程
+     * 堆排序与快速排序，归并排序一样都是时间复杂度为O(N*logN)的几种常见排序方法
+     *
+     * @param ints   数组
+     * @param root   根节点
+     * @param length 数组长度
+     */
+    private void heapAdjust(int[] ints, int root, int length) {
 
+        for (int i = root * 2 + 1; i < length; i++) {
+            //若左子结点 root*2+1 小于右子节点 root*2+2 则值交换
+            swap(ints, i, i + 1);
+            // 父节点与左子节点比较 并进行直接换
+            swap(ints, root, i);
+        }
+    }
+
+    private void swap(int[] ints, int index1, int index2) {
+        if (ints[index1] < ints[index2]) {
+            int temp = ints[index1];
+            ints[index1] = ints[index2];
+            ints[index2] = temp;
+        }
     }
 }
